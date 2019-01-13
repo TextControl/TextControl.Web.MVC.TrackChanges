777ac90bcd451a861b2d21e02b95f1b3c82df44a
Bjoern Meyer <bjoern@textcontrol.com>
2019-01-13 18:12:35 +0100

    Initial upload

---
+++ tx_visualizechanges/Controllers/HomeController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using tx_visualizechanges.Models;
+using TXTextControl;
+
+namespace tx_visualizechanges.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            // set document name to dummy document
+            string sDocumentName = Server.MapPath("~/template.docx");
+
+            // create a new TrackedChangeModel
+            TrackedChangeModel model = new TrackedChangeModel() {
+                DocumentName = sDocumentName
+            };
+        
+            // use a temporary ServerTextControl to retrieve the
+            // changes and to create the thumbnails
+            using (ServerTextControl tx = new ServerTextControl())
+            {
+                tx.Create();
+
+                // load the document
+                tx.Load(sDocumentName, StreamType.WordprocessingML);
+
+                // loop through all text parts including headers and footers
+                foreach (IFormattedText textPart in tx.TextParts)
+                {
+                    // lkoop through all changes
+                    foreach (TXTextControl.TrackedChange trackedChange in textPart.TrackedChanges)
+                    {
+                        if (trackedChange.ChangeTime < model.FirstChange) // get first change time stamp
+                            model.FirstChange = trackedChange.ChangeTime;
+
+                        // create a new TrachedChange object
+                        Models.TrackedChange tc = new Models.TrackedChange()
+                        {
+                            ChangeTime = trackedChange.ChangeTime,
+                            HighlightColor = ColorTranslator.ToHtml(trackedChange.HighlightColor),
+                            ChangeKind = (Models.ChangeKind)trackedChange.ChangeKind,
+                            Text = trackedChange.Text,
+                            UserName = (trackedChange.UserName == "") ? "Unknown User" : trackedChange.UserName
+                        };
+
+                        // highlight the change in the document
+                        trackedChange.ScrollTo();
+                        trackedChange.Select();
+                        textPart.Selection.TextBackColor = Color.Yellow;
+
+                        // create a thumbnail of the page and the highlighted change
+                        tc.Image = ImageToBase64(
+                            tx.GetPages().GetItem().GetImage(100, Page.PageContent.All)
+                        , ImageFormat.Jpeg);
+
+                        // add the change to the model
+                        model.TrackedChanges.Add(tc);
+                    }
+                }
+            }
+
+            return View(model);
+        }
+
+        public string ImageToBase64(System.Drawing.Image image, ImageFormat format)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                image.Save(ms, format);
+                byte[] imageBytes = ms.ToArray();
+                string base64String = Convert.ToBase64String(imageBytes);
+                return base64String;
+            }
+        }
+    }
+}
---
+++ tx_visualizechanges/Models/TrackedChangeModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+
+namespace tx_visualizechanges.Models
+{
+    public class TrackedChangeModel
+    {
+        public string DocumentName { get; set; }
+        public DateTime FirstChange { get; set; } = DateTime.MaxValue;
+        public List<TrackedChange> TrackedChanges { get; set; } = new List<TrackedChange>();
+    }
+
+    public class TrackedChange
+    {
+        public string UserName { get; set; }
+        public DateTime ChangeTime { get; set; }
+        public string Text { get; set; }
+        public string HighlightColor { get; set; }
+        public ChangeKind ChangeKind { get; set; }
+        public string Image { get; set; }
+    }
+
+    public enum ChangeKind
+    {
+        InsertedText = 4096,
+        DeletedText = 8192
+    }
+}
